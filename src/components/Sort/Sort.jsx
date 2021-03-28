81829f3aa0b5c8199457343c2e9d8de4ea9d72e0
Frostnn <gradysnn92@yandex.ru>
2021-03-28 22:01:49 +0300

    add component sort

---
+++ src/components/Sort/Sort.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState, useRef } from 'react';
+import classNames from 'classnames';
+const Sort = ({ sort }) => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [active, setActive] = useState(null);
+  const [visiblePopupName, setVisiblePopupName] = useState('популярности');
+  const sortRef = useRef(null);
+
+  const toggleVisiblePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
+  const toggleActiveClass = (index, sorts) => {
+    return setActive(index), setVisiblePopupName(sorts);
+  };
+
+  const handleOutsideClick = (e) => {
+    console.log(e.target);
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('click', handleOutsideClick);
+    console.log(sortRef.current);
+  }, []);
+  return (
+    <div className="sort" ref={sortRef}>
+      <div className="sort__label">
+        <svg
+          width="10"
+          height="6"
+          viewBox="0 0 10 6"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
+            fill="#2C2C2C"
+          />
+        </svg>
+        <b>Сортировка по:</b>
+        <span onClick={toggleVisiblePopup}>{visiblePopupName}</span>
+      </div>
+      {showPopup && (
+        <div className="sort__popup">
+          <ul>
+            {sort &&
+              sort.map((sorts, index) => (
+                <li
+                  key={`${sorts}_${index}`}
+                  className={classNames(active === index ? 'active' : '')}
+                  onClick={() => toggleActiveClass(index, sorts)}>
+                  {sorts}
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Sort;
